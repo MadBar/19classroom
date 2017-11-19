f51ab219b2c7ff3acc0ca1777c4f72b31e5715b3
MadBar <madeleinetrollvik@gmail.com>
2017-11-19 19:19:40 +0100

    Initial commit

---
+++ src/main/java/com/example/classroom/Person.java
@@ -0,0 +1,16 @@
+package com.example.classroom;
+
+public class Person {
+	private int age = 0;
+	private String firstName = "";
+	private String lastName = "";
+	private char gender = ' ';
+
+	public Person(int age, String firstName, String lastName, char gender) {
+
+		this.age = age;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+	}
+}
---
+++ src/main/java/com/example/classroom/Student.java
@@ -0,0 +1,5 @@
+package com.example.classroom;
+
+public class Student {
+
+}
