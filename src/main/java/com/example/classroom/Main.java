76e5162d39bdf0f31c9a4821487966bf9bd28421
MadBar <madeleinetrollvik@gmail.com>
2017-11-19 21:30:46 +0100

    Main work. App runs, but not as desired and not finished

--- src/main/java/com/example/classroom/Main.java
+++ src/main/java/com/example/classroom/Main.java
@@ -5,12 +5,20 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		ArrayList<Student> stundentsAL = new ArrayList<Student>();
-		
-		Classroom cr = new Classroom("Java1", "Spring2018", stundentsAL);
-		
 
+		ArrayList<Student> stundentsAL = new ArrayList<Student>();
+
+		Classroom cr = new Classroom("Java1", "Spring2018", stundentsAL);
+
+		Student stud1 = new Student(18, "Jenny", "Hoverman", 'F');
+		Student stud2 = new Student(21, "Henry", "Anatano", 'M');
+		Student stud3 = new Student(18, "Emmet", "Trollqvist", 'M');
+		
+		cr.addANewStudent(stud1);
+		cr.addANewStudent(stud2);
+		cr.addANewStudent(stud3);
+		
+		cr.printFullRelatory();
 	}
 
 }
