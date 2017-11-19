3d04584327053af9c1dcfe7c3b40f714b1c413fc
MadBar <madeleinetrollvik@gmail.com>
2017-11-19 21:15:42 +0100

    Work on Classrome.java

--- src/main/java/com/example/classroom/Classroom.java
+++ src/main/java/com/example/classroom/Classroom.java
@@ -3,19 +3,42 @@ package com.example.classroom;
 import java.util.ArrayList;
 
 public class Classroom {
-	private String classroomName;
-	private String classroomTerm;
-	private ArrayList<Student> students;
+	private String classroomName = "Kvalit17";
+	private String classroomTerm = "Fall2017";
+	// How declare arraylist?
+	private ArrayList<Student> students = new ArrayList<Student>();
 
-	// ??
-	public String removeAStudent(String firstName) {
-		return firstName;
+	public Classroom(String classroomName, String classroomTerm, ArrayList<Student> students) {
+		this.classroomName = classroomName;
+		this.classroomTerm = classroomTerm;
+		this.students = students;
+	}
+
+	// ?? I added void, makes sense to me. Solve syntax! 
+	public void removeAStudent(String firstName) {
+
+		// This could possibly be in if statement?
+		// Search students arraylist for firstName
+
+		// if(student exist in arraylist){
+		// remove student from arrayList
+		// return System.out.println("Student "+ firstName + "was successfully
+		// removed.");
+		// }else{
+		// System.out.println("The student does not exist!");
+		// }
 
 	}
 
-	// ??
-	public Student addANewStudent(Student student) {
-		return student;
+	// ?? says nothing about void but doesnt seem to need to return.. I add void.
+	public void addANewStudent(Student student) {
+
+		this.students.add(student);
+
+		// Next line not needed?
+		// setStudents(students);
+
+		// return student;
 
 	}
 
